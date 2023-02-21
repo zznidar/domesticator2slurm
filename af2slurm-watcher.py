2f09104dd732b118117be243b385468682dfe516
Ajasja Ljubetic <ajasja.ljubetic@gmail.com>
2023-02-21 18:19:56 +0100

    Testing out config parser

--- af2slurm-watcher.py
+++ af2slurm-watcher.py
@@ -5,9 +5,12 @@ from argparse import ArgumentParser
 
 if __name__ == "__main__":
     parser = ArgumentParser(
-        prog="af2slurm-watcher",
-        description="Watches a folder for fasta files and submits them to slurm"
+        prog="af2slurm-watcher", description="Watches a folder for fasta files and submits them to slurm"
     )
     parser.add_argument("--config", help="Path to config file", type=str, default="af2slurm.config")
     args = parser.parse_args()
-    print(args.config)
+
+    #print(args.config)
+    cf = ConfigParser()
+    cf.read(args.config)
+    #print(cf.get("APP", "in_folder"))
