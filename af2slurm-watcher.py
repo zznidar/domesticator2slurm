719b181ed4426fbc9e9fc89833b2ca274e298a79
Ajasja Ljubetic <ajasja.ljubetic@gmail.com>
2023-02-21 18:13:21 +0100

    Scaffold commit Basic arguments and configuration values added

---
+++ af2slurm-watcher.py
@@ -0,0 +1,13 @@
+#!python
+from configparser import ConfigParser
+from argparse import ArgumentParser
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        prog="af2slurm-watcher",
+        description="Watches a folder for fasta files and submits them to slurm"
+    )
+    parser.add_argument("--config", help="Path to config file", type=str, default="af2slurm.config")
+    args = parser.parse_args()
+    print(args.config)
